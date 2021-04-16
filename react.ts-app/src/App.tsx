3b49bcb32150a5be4f89335cfbca89fbf99d89ae
nakanishi <nakanishi_kanta@hasigo.co.jp>
2021-04-16 12:42:12 +0900

    feature: Hooks

--- react.ts-app/src/App.tsx
+++ react.ts-app/src/App.tsx
@@ -29,7 +29,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Redux
+            Hooks
           </a>
           <span>, </span>
           <a
